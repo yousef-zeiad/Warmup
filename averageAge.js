d4893a9c1d78d2519cb5def0d5adb59a6222cd15
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-16 20:04:27 +0200

    averageAge

---
+++ averageAge.js
@@ -0,0 +1,17 @@
+// Write a function called  averageAge that accpets an array of objects and return the average ages for the people who are between 18 and 50
+ 
+
+ var people = [ 
+       {name: {first: 'Ahmad', middle: 'B.', last: 'AlAhmad'}, age: 85}, 
+       {name: {first: 'Amer', last: 'MHD'}, age: 43}, 
+       {name: {first: 'Aya', last: 'Sultan'}, age: 36}, 
+       {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 44}, 
+       {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 54}, 
+       {name: {first: 'Lina', last: 'MHD'}, age: 14} ,
+       {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
+ ]; 
+ function averageAge(people) { 
+       // YOUR CODE HERE 
+ }
+
+ averageAge(people); // 43+36+44+24 = 36.74
