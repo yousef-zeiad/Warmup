97677e584141c46220181a2f46a8550cc12c034c
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-19 07:56:41 +0200

    closestMultipleOf10

---
+++ closestMultipleOf10.js
@@ -0,0 +1,8 @@
+ // Given a number return the closest number to it that is divisible by 10.
+ // Example :
+	// 22 ===> 20
+	// 25 ===> 30
+	// 37 ===> 40
+ function closestMultipleOf10(num) {
+ 	//your code is here
+ }
