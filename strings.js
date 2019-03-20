c3a6db6c7be060b75acc5d12e8dada397e5a163d
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-20 09:12:55 +0200

    strings

---
+++ strings.js
@@ -0,0 +1,7 @@
+// Write a function called repeatStr which repeats the given
+// string string exactly n times.
+// repeatStr("hello",4) ==> 'hellohellohellohello'
+	
+	function repeatStr (n, s) {
+		//your code is here
+	}
