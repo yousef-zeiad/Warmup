d4b8d1e136793866b30c8979bb6b4bc09b30d3fb
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-25 08:44:51 +0200

    init

---
+++ Oop.js
@@ -0,0 +1,30 @@
+// We have this code from Adding Methods With Closures lecture: 
+// Refactor the code as an MakeGame class that shares its methods across different instances.
+
+
+function randInt(n) {
+    return Math.floor(Math.random() * (n + 1));
+}
+
+function makeGame(upperbound){
+  var upper=upperbound;
+  var rand=randInt(upper)
+  var counter=0;
+  return {
+  	guessMyNumber:function(n){
+  		counter++;
+	    if (n > upper) {
+	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+	    } else if (n === rand) {
+	      return "You guessed my number!";
+	    }
+	    return "Nope! That wasn't it!";
+  	},
+  	giveUp:function(){
+    	return rand;
+  	},
+  	numOfGuesses:function(){
+    	return counter;
+  	}
+  }
+}
