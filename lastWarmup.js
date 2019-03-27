90587210c21fbb12efae0ebe41a9322713bc306c
SaharAl-shurafat1990 <hayelsahar@gmail.com>
2019-03-27 08:46:38 +0200

    lastWarmup

---
+++ lastWarmup.js
@@ -0,0 +1,10 @@
+/* 
+Given an array of integers, return indices of the two numbers
+such that they add up to a specific target.
+You may assume that each input would have exactly one solution.
+Example:
+var nums = [2, 7, 11, 15], target = 9,
+twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
+*/
+
+
